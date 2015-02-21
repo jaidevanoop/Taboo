a4f2be59fda8e94f7fc1e39d882eee90bd75b9a5
Jaidev Anoop <jaidev.anoop@gmail.com>
2015-02-22 00:33:04 +0530

    Create BlankPage2.xaml.cs

---
+++ BlankPage2.xaml.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
+
+namespace App1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class BlankPage2 : Page
+    {
+        int i=0;
+        public BlankPage2()
+        {
+            this.InitializeComponent();
+            Windows.Phone.UI.Input.HardwareButtons.BackPressed += HardwareButtons_BackPressed;
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+        private void HardwareButtons_BackPressed(object sender, Windows.Phone.UI.Input.BackPressedEventArgs e)
+        {
+            Frame rootFrame = Window.Current.Content as Frame;
+            if (rootFrame != null && rootFrame.CanGoBack)
+            {
+                e.Handled = true;
+                rootFrame.GoBack();
+            }
+
+        }
+        
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            i++;
+            if (i == 1)
+            { Rule.Text = "Once the team correctly guesses the word exactly as written on the card, the giver moves on to the next word, trying to get as many words as possible in the allotted time."; }
+            if(i==2)
+            { Rule.Text = "For every hit, you get +2 points and -1 for every miss. May the force be with you!";
+            Btn1.Content = "Main";
+            }
+            if(i==3)
+            { this.Frame.Navigate(typeof(MainPage));}
+        }
+    }
+}
