711020614537a19a911a2498effda49009f4f743
Jaidev Anoop <jaidev.anoop@gmail.com>
2015-02-22 00:28:07 +0530

    Update BlankPage1.xaml.cs

--- BlankPage1.xaml.cs
+++ BlankPage1.xaml.cs
@@ -20,14 +20,39 @@ namespace App1
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
+    /// 
+    
+
     public sealed partial class BlankPage1 : Page
     {
+        int score = 0;
+
+
+
+
         public BlankPage1()
         {
             this.InitializeComponent();
             Windows.Phone.UI.Input.HardwareButtons.BackPressed += HardwareButtons_BackPressed;
         }
-
+        DispatcherTimer dispatcherTimer;
+        int timesTicked = 60;
+        public void dispatcherTimerSetup()
+        {
+            dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += dispatcherTimer_Tick;
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Start();
+        }
+        void dispatcherTimer_Tick(object sender, object e)
+        {
+            Countdown.Text = timesTicked.ToString();
+            timesTicked--;
+            if(timesTicked==0)
+            {
+                this.Frame.Navigate(typeof(MainPage));
+            }
+        }
         /// <summary>
         /// Invoked when this page is about to be displayed in a Frame.
         /// </summary>
@@ -35,6 +60,7 @@ namespace App1
         /// This parameter is typically used to configure the page.</param>
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
+            dispatcherTimerSetup();
         }
         
         private void HardwareButtons_BackPressed(object sender, Windows.Phone.UI.Input.BackPressedEventArgs e)
@@ -47,11 +73,38 @@ namespace App1
             }
 
         }
-        
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            TextBlock tb5 = new TextBlock();
-            tb5.Text = "Success";
+            score = score + 5;
+            tb5.Text="Score: " + score;
         }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            score = score - 2;
+            tb5.Text = "Score: " + score;
+        }
+        //----
+        /*
+        DispatcherTimer timer1 = new DispatcherTimer();
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            timer1.Interval = new TimeSpan(0, 0, 0, 1);
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Start();
+        }
+
+        int tik = 60;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Countdown.Text = tik + " Seconds Remaining";
+            if (tik > 0)
+                tik--;
+            else
+                Countdown.Text = "Times Up";
+        }
+         */
+        
     }
 }
