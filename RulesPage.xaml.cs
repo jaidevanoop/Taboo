5aeef3dc64a0537f6bd8be6f8adcf394c06d0aef
Vidit Bhargava <viditvineet@yahoo.co.in>
2015-02-22 02:17:46 +0530

    Update and rename Rule.xaml.cs to RulesPage.xaml.cs

--- RulesPage.xaml.cs
+++ RulesPage.xaml.cs
@@ -12,23 +12,21 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
-
-namespace App1
+namespace Taboo
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class BlankPage2 : Page
+    public sealed partial class RulesPage : Page
     {
-        int i=0;
-        public BlankPage2()
+        int i = 0;
+
+        public RulesPage()
         {
             this.InitializeComponent();
             Windows.Phone.UI.Input.HardwareButtons.BackPressed += HardwareButtons_BackPressed;
         }
-
         /// <summary>
         /// Invoked when this page is about to be displayed in a Frame.
         /// </summary>
@@ -45,20 +43,19 @@ namespace App1
                 e.Handled = true;
                 rootFrame.GoBack();
             }
-
         }
-        
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             i++;
             if (i == 1)
             { Rule.Text = "Once the team correctly guesses the word exactly as written on the card, the giver moves on to the next word, trying to get as many words as possible in the allotted time."; }
-            if(i==2)
-            { Rule.Text = "For every hit, you get +2 points and -1 for every miss. May the force be with you!";
-            Btn1.Content = "Main";
+            if (i == 2)
+            {
+                Rule.Text = "For every hit, you get +2 points and -1 for every miss. May the force be with you!";
+                Btn1.Content = "Main";
             }
-            if(i==3)
-            { this.Frame.Navigate(typeof(MainPage));}
+            if (i == 3)
+            { this.Frame.Navigate(typeof(MainPage)); }
         }
     }
 }
